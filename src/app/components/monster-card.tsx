6a60f18a0196e1f1f431828967f42ea17d1459fa
Holliewood67 <ethancantrell67@gmail.com>
2024-10-28 22:33:05 -0500

    made monster cards look less like poo poo

--- src/app/components/monster-card.tsx
+++ src/app/components/monster-card.tsx
@@ -9,11 +9,11 @@ export default function MonsterCard(
     }) {
     return(
         <Link href={`/epk/${props.urlPath}`}>
-            <div className="monster-card border-2 border-yellow-400 rounded my-5 hover:scale-110">
-                <div className="monster-card-img border-4 border-yellow-900">
-                    <Image src={`/epkpics/${props.imgPath}`} alt="Ethan Cantrell" width={800} height={1000} />
+            <div className="monster-card  rounded my-5 hover:scale-110">
+                <div className="monster-card-img">
+                    <Image className="rounded-3xl" src={`/epkpics/${props.imgPath}`} alt="Ethan Cantrell" width={800} height={1000} />
                 </div>
-                <div className="bg-yellow-500 text-white flex items-center justify-center"><h1 className="flex justify-center w-[80%] text-base bg-yellow-300 my-2  border-2 border-black border-rounded rounded-full text-black">{props.monsterName}</h1></div>
+                <div className=" text-white flex items-center justify-center"><h1 className="flex justify-center w-[80%] text-base bg-yellow-400 my-2  border-2 border-black border-rounded rounded-full text-black">{props.monsterName}</h1></div>
             </div>
         </Link>
     )
